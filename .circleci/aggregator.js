3ebaa3308e8ae1a1a8e7b5734c5dc67b4acdacff
Sergey Nikitin <srg.post@gmail.com>
2019-08-13 02:01:30 +0700

    fix(circleci): Run the same container

---
+++ .circleci/aggregator.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+process.env.ARTIFACTS = `${process.env.ARTIFACTS}1`;
--- .circleci/bot.js
+++ .circleci/bot.js
@@ -2,12 +2,22 @@
 
 const bot = require('circle-github-bot').create();
 
+const username = process.env.CIRCLE_PROJECT_USERNAME;
+const vcs = 'github';
+const project = process.env.CIRCLE_PROJECT_REPONAME;
+const build = process.env.CIRCLE_BUILD_NUM;
+const token = 'b7af2c8069b9bbd480aef1f7cdbe02e2b74f9873';
+const artifacts = process.env.ARTIFACTS;
+console.log(username, vcs, project, build, token, artifacts);
 bot.comment(
 	process.env.GH_AUTH_TOKEN,
-	`
+	`${artifacts}
 <h3>${bot.commitMessage()}</h3>
 <div>Coverage: <strong>${bot.artifactLink('coverage/index.html', 'report')}</strong></div>
 <div>Test results: <strong>${bot.artifactLink('test-report/index.html', 'report')}</strong></div>
-
+<div>Semantic release info: <strong>${bot.artifactLink(
+		'semantic-release/index.log',
+		'log'
+	)}</strong></div>
 `
 );
